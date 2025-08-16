b5824498b95524f819eaa990c6e4c3fcf740480a
Alx1as <alexiarubin23@gmail.com>
2025-08-16 16:22:25 -0300

    Resuelto hasta el 7

---
+++ Programación_Estructurada/src/programación_estructurada/Ejercicio08_precioFInal.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package programación_estructurada;
+
+/**
+ *
+ * @author alexia
+ * 8.​ Cálculo del Precio Final con impuesto y descuento.
+ Crea un método calcularPrecioFinal(double impuesto, double
+descuento) que calcule el precio final de un producto en un e-commerce. La
+fórmula es:
+PrecioFinal = PrecioBase + (PrecioBase×Impuesto) − (PrecioBase×Descuento)
+PrecioFinal = PrecioBase + (PrecioBase \times Impuesto) - (PrecioBase \times
+Descuento)
+Desde main(), solicita el precio base del producto, el porcentaje de
+impuesto y el porcentaje de descuento, llama al método y muestra el precio
+final.
+
+ */
+import java.util.Scanner;
+public class Ejercicio08_precioFInal {
+    public static void main(String[] args){
+        Scanner input = new Scanner (System.in);
+        
+    }
+}
